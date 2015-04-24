dbe72a2d7d2aa8b05323bc3a567c35f79a8fc7e9
Alice <alice@pelotoncycle.com>
2015-04-23 23:54:12 -0400

    made tags section

---
+++ JuSH/InterestTag.swift
@@ -0,0 +1,15 @@
+//
+//  InterestTag.swift
+//  JuSH
+//
+//  Created by Alice Q Wong on 4/23/15.
+//  Copyright (c) 2015 Alice Q Wong. All rights reserved.
+//
+
+import UIKit
+
+class InterestTag: UICollectionViewCell {
+
+    @IBOutlet weak var tagLabel: UILabel!
+ 
+}
---
+++ JuSH/TagCollectionController.swift
@@ -0,0 +1,92 @@
+//
+//  TagCollectionController.swift
+//  JuSH
+//
+//  Created by Alice Q Wong on 4/23/15.
+//  Copyright (c) 2015 Alice Q Wong. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "Tag"
+
+class TagCollectionController: UICollectionViewController {
+    
+    let myInterests = ["running", "food", "hiking", "basketball"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+    //    self.collectionView!.registerClass(InterestTag.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myInterests.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! InterestTag
+        cell.tagLabel?.text = myInterests[indexPath.item]
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
