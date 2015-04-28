6bfa3a0802cabc433bac519adfd55afed8362564
Alice <alice@pelotoncycle.com>
2015-04-28 19:59:37 -0400

    preferences screen - resolved #10

---
+++ JuSH/PreferencesTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PreferencesTableViewController.swift
+//  JuSH
+//
+//  Created by Alice Q Wong on 4/28/15.
+//  Copyright (c) 2015 Alice Q Wong. All rights reserved.
+//
+
+import UIKit
+
+class PreferencesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
